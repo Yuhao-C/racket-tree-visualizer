c0807949b51543ffc8ce0c7829d26f81cd0ea3ad
yuhaochen0107@gmail.com <yuhaochen0107@gmail.com>
2020-01-13 21:12:57 -0500

    binary-tree fixed

--- tree-visualizer.rkt
+++ tree-visualizer.rkt
@@ -5,15 +5,20 @@
 
 (provide visualize)
 
-(define (binary->generic tree)
+(define (binary->layout tree)
   (cond
-    [(empty? tree) empty]
-    [else (begin
-            (define v (struct->vector tree))
-            (define key (vector-ref v 1))
-            (define left (vector-ref v 2))
-            (define right (vector-ref v 3))
-            (cons key (list (binary->generic left) (binary->generic right))))]))
+    [(empty? tree) (tree-layout #:pict (blank))]
+    [else (define v (struct->vector tree))
+          (define key (vector-ref v 1))
+          (define left (vector-ref v 2))
+          (define right (vector-ref v 3))
+          (tree-layout #:pict (text (~v key) null 14 0)
+                       (if (empty? left)
+                           #f
+                           (binary->layout left))
+                       (if (empty? right)
+                           #f
+                           (binary->layout right)))]))
 
 (define (cons->generic tree) tree)
 
@@ -37,20 +42,22 @@
     [(empty? tree)
      (tree-layout #:pict (blank))]
     [(not (list? tree))
-     (tree-layout #:pict (text (~v tree)))]
+     (tree-layout #:pict (text (~v tree) null 14 0))]
     [else
      (apply tree-layout
-            #:pict (text (~v (car tree)))
+            #:pict (text (~v (car tree)) null 14 0)
             (map tree->tree-layout (cdr tree)))]))
 
 (define handler-hash (hash
-                        'binary-tree binary->generic
-                        'cons-tree cons->generic
-                        'list-tree list->generic
-                        'struct-tree struct->generic))
+                      'binary-tree binary->layout
+                      'cons-tree cons->generic
+                      'list-tree list->generic
+                      'struct-tree struct->generic))
 
 (define (visualize tree-type tree)
   (pict->bitmap
-    (naive-layered
-      (tree->tree-layout
-        ((hash-ref handler-hash tree-type) tree)))))
+   (naive-layered
+    (if (symbol=? tree-type 'binary-tree)
+        ((hash-ref handler-hash tree-type) tree)
+        (tree->tree-layout
+         ((hash-ref handler-hash tree-type) tree))))))
